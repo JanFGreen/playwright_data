c2081f462913b933ab1b06ec0519f229de8e1699
Krzysiek <kkijas@jaktestowac.pl>
2023-08-09 14:53:12 +0200

    feat: full S01 and S02

---
+++ S02/L09/tests/login.spec.ts
@@ -0,0 +1,23 @@
+import { LoginPage } from '../src/pages/login.page';
+import { WelcomePage } from '../src/pages/welcome.page';
+import { testUser1 } from '../src/test-data/user.data';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify login', () => {
+  test('login with correct credentials @GAD-R02-01', async ({ page }) => {
+    // Arrange
+    const userEmail = testUser1.userEmail;
+    const userPassword = testUser1.userPassword;
+    const loginPage = new LoginPage(page);
+
+    // Act
+    await loginPage.goto();
+    await loginPage.login(userEmail, userPassword);
+
+    const welcomePage = new WelcomePage(page);
+    const title = await welcomePage.title();
+
+    // Assert
+    expect(title).toContain('Welcome');
+  });
+});
