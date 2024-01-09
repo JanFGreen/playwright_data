bedf898d4bc57c9150252cee084da8093f1e41bf
Krzysiek <kkijas@jaktestowac.pl>
2024-01-09 15:17:50 +0100

    feat: code for S10

---
+++ S10-api-response/L05-wait-timeout-status/tests/integration/articles.spec.ts
@@ -0,0 +1,72 @@
+import { RESPONSE_TIMEOUT } from '@_pw-config';
+import { prepareRandomArticle } from '@_src/factories/article.factory';
+import { expect, test } from '@_src/fixtures/merge.fixture';
+
+test.describe('Verify articles', () => {
+  test('reject creating article without title @GAD-R04-01 @logged', async ({
+    addArticleView,
+    page,
+  }) => {
+    // Arrange
+    const expectedErrorMessage = 'Article was not created';
+    const expectedResponseCode = 422;
+
+    const articleData = prepareRandomArticle();
+    articleData.title = '';
+
+    const responsePromise = page.waitForResponse('/api/articles', {
+      timeout: RESPONSE_TIMEOUT,
+    });
+    // Act
+    await addArticleView.createArticle(articleData);
+    const response = await responsePromise;
+
+    // Assert
+    await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage);
+    expect(response.status()).toBe(expectedResponseCode);
+  });
+
+  test('reject creating article without body @GAD-R04-01 @logged', async ({
+    addArticleView,
+  }) => {
+    // Arrange
+    const expectedErrorMessage = 'Article was not created';
+    const articleData = prepareRandomArticle();
+    articleData.body = '';
+
+    // Act
+    await addArticleView.createArticle(articleData);
+
+    // Assert
+    await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage);
+  });
+
+  test.describe('title length', () => {
+    test('reject creating article with title exceeding 128 signs @GAD-R04-02 @logged', async ({
+      addArticleView,
+    }) => {
+      // Arrange
+      const expectedErrorMessage = 'Article was not created';
+      const articleData = prepareRandomArticle(129);
+
+      // Act
+      await addArticleView.createArticle(articleData);
+
+      // Assert
+      await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage);
+    });
+
+    test('create article with title with 128 signs @GAD-R04-02 @logged', async ({
+      addArticleView,
+    }) => {
+      // Arrange
+      const articleData = prepareRandomArticle(128);
+
+      // Act
+      const articlePage = await addArticleView.createArticle(articleData);
+
+      // Assert
+      await expect.soft(articlePage.articleTitle).toHaveText(articleData.title);
+    });
+  });
+});
---
+++ S10-api-response/L10-solution-wait-comments/playwright.config.ts
@@ -0,0 +1,49 @@
+import { BASE_URL } from '@_config/env.config';
+import { defineConfig, devices } from '@playwright/test';
+import * as path from 'path';
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+
+export const STORAGE_STATE = path.join(__dirname, 'tmp/session.json');
+export const RESPONSE_TIMEOUT = 10_000;
+
+export default defineConfig({
+  testDir: './tests',
+  globalSetup: 'config/global.setup.ts',
+  timeout: 60_000,
+  expect: { timeout: 10_000 },
+  fullyParallel: true,
+  retries: 0,
+  workers: undefined,
+  reporter: 'html',
+  use: {
+    baseURL: BASE_URL,
+    actionTimeout: 0,
+    trace: 'on',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'chromium-non-logged',
+      grepInvert: /@logged/,
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'setup',
+      testMatch: '*.setup.ts',
+    },
+    {
+      name: 'chromium-logged',
+      grep: /@logged/,
+      dependencies: ['setup'],
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: STORAGE_STATE,
+      },
+    },
+  ],
+});
