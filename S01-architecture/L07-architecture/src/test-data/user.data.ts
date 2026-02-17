3965d1fed6ee7a011e903eb4400b3c57edf8edee
Jan Zielinski <jan.f.zielinski@gmail.com>
2026-02-17 10:58:21 +0100

    wig: refactor and use test data

---
+++ S01-architecture/L07-architecture/src/test-data/user.data.ts
@@ -0,0 +1,6 @@
+import { LoginUser } from '../models/loginUser.models';
+
+export const loginuser1: LoginUser = {
+  email: 'Moses.Armstrong@Feest.ca',
+  password: 'test1',
+};
--- S01-architecture/L07-architecture/tests/login.spec.ts
+++ S01-architecture/L07-architecture/tests/login.spec.ts
@@ -1,5 +1,6 @@
 import { LoginPage } from '../src/pages/login.page';
 import { WelcomePage } from '../src/pages/welcome.page';
+import { loginuser1 } from '../src/test-data/user.data';
 import { expect, test } from '@playwright/test';
 
 test('Login with correct credentials @GAD_R02_01', async ({ page }) => {
@@ -8,14 +9,15 @@ test('Login with correct credentials @GAD_R02_01', async ({ page }) => {
   // const email = 'Moses.Armstrong@Feest.ca';
   // const password = 'test1';
 
-  const userLoginData = {
-    email: 'Moses.Armstrong@Feest.ca',
-    password: 'test1',
-  };
+  // Move data to test data + interface reduce imports
+  // const userLoginData = {
+  //   email: 'Moses.Armstrong@Feest.ca',
+  //   password: 'test1',
+  // };
 
   //Act
   await loginPage.goto();
-  await loginPage.login(userLoginData);
+  await loginPage.login(loginuser1);
 
   const welcomePage = new WelcomePage(page);
   const title = await welcomePage.getTitle();
